ebdc9ba92dcad2588548611c200ad3463fc65f79
Alexey Kazantsev <a.kazantsev@samsung.com>
2015-06-01 15:09:31 +0300

    Modified Veles config to match Torch

--- veles/benchmark.py
+++ veles/benchmark.py
@@ -21,8 +21,8 @@ base_lr = 0.01
 wd = 0.0005
 root.alexnet.update({
     "layers": [{"type": "conv_str",
-                "->": {"n_kernels": 96, "kx": 11, "ky": 11,
-                       "padding": (0, 0, 0, 0), "sliding": (4, 4),
+                "->": {"n_kernels": 64, "kx": 11, "ky": 11,
+                       "padding": (2, 2, 2, 2), "sliding": (4, 4),
                        "weights_filling": "gaussian", "weights_stddev": 0.01,
                        "bias_filling": "constant", "bias_stddev": 0},
                 "<-": {"learning_rate": base_lr,
@@ -36,7 +36,7 @@ root.alexnet.update({
                # {"type": "zero_filter",
                #  "grouping": 2},
                {"type": "conv_str",
-                "->": {"n_kernels": 256, "kx": 5, "ky": 5,
+                "->": {"n_kernels": 192, "kx": 5, "ky": 5,
                        "padding": (2, 2, 2, 2), "sliding": (1, 1),
                        "weights_filling": "gaussian", "weights_stddev": 0.01,
                        "bias_filling": "constant", "bias_stddev": 0.1},
@@ -61,7 +61,7 @@ root.alexnet.update({
 
                # {"type": "zero_filter", "grouping": 2},
                {"type": "conv_str",
-                "->": {"n_kernels": 384, "kx": 3, "ky": 3,
+                "->": {"n_kernels": 256, "kx": 3, "ky": 3,
                        "padding": (1, 1, 1, 1), "sliding": (1, 1),
                        "weights_filling": "gaussian", "weights_stddev": 0.01,
                        "bias_filling": "constant", "bias_stddev": 0.1},
@@ -83,7 +83,7 @@ root.alexnet.update({
                {"type": "max_pooling",
                 "->": {"kx": 3, "ky": 3, "sliding": (2, 2)}},
 
-               {"type": "all2all_relu",
+               {"type": "all2all",
                 "->": {"output_sample_shape": 4096,
                        "weights_filling": "gaussian", "weights_stddev": 0.005,
                        "bias_filling": "constant", "bias_stddev": 1},
@@ -93,7 +93,7 @@ root.alexnet.update({
                        "gradient_moment": 0.9, "gradient_moment_bias": 0.9}},
                # {"type": "dropout", "dropout_ratio": 0.5},
 
-               {"type": "all2all_relu",
+               {"type": "all2all",
                 "->": {"output_sample_shape": 4096,
                        "weights_filling": "gaussian", "weights_stddev": 0.005,
                        "bias_filling": "constant", "bias_stddev": 1},
@@ -247,10 +247,12 @@ def main():
             if first:
                 wf.generate_graph("alexnet.svg")
             wf.run()
-            logging.info("Forward pass: %.3f",
-                         (wf.t1.times[-1] - wf.t1.times[1]) / (wf.t1.n_it - 1))
-            logging.info("Backward pass: %.3f",
-                         (wf.t3.times[-1] - wf.t3.times[1]) / (wf.t3.n_it - 1))
+            logging.info("Forward pass: %.2f msec",
+                         1000.0 * (wf.t1.times[-1] - wf.t1.times[1]) /
+                         (wf.t1.n_it - 1))
+            logging.info("Backward pass: %.2f msec",
+                         1000.0 * (wf.t3.times[-1] - wf.t3.times[1]) /
+                         (wf.t3.n_it - 1))
 
             logging.info("\n%s: benchmark ended for dtype %s\n",
                          device_class, dtype)
